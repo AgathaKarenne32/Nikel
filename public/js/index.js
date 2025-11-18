252cd1edeb4b85bdb462a3a55763485cbfeed94a
Agatha Karenne <142344001+AgathaKarenne32@users.noreply.github.com>
2025-11-18 18:50:39 +0000

    criando criterios de email e senha

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,36 @@
+const myModal = new bootstrap.Modal("#register-modal");
+
 //criar conta
-document.getElementById("create-form").addEventListener("submit", function(e){
-    e.preventDefault()
-})
+document.getElementById("create-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+
+    console.log("Entrou na função de enviar!");
+
+    const email = document.getElementById("email-create-input").value;
+    const password = document.getElementById("password-create-input").value;
+
+    if (email.length < 5) {
+        alert("Preencha o campo com um e-mail válido.");
+        return;
+    }
+
+    if (password.length < 4) {
+        alert("A senha deve ter no mínimo 4 caracteres.");
+        return;
+    }
+
+    saveAccount({
+        login: email,
+        password: password,
+        transactions: []
+    });
+    myModal.hide();
+
+    alert("Conta criada com sucesso!");
+
+});
+
+function saveAccount(data) {
+    console.log(data);
+}

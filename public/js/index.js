453e3ce6069aff9f0447c91941ad312e10f8f839
Agatha Karenne <agathakarenne32@gmail.com>
2025-11-19 00:13:18 -0300

    criada checkslogged, login no sistema e sessionStorage da login usuario...

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,39 @@
 const myModal = new bootstrap.Modal("#register-modal");
+let logged = sessionStorage.getItem("logged");
+const session = localStorage.getItem("session");
+
+checkLogged();
+
+//logar no sistema
+document.getElementById("login-form").addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const email = document.getElementById("email-input").value;
+    const password = document.getElementById("password-input").value;
+    const checksession = document.getElementById("session-check").checked;
+
+    const account = getAccount(email);
+
+    console.log("Email digitado:", email);
+    console.log("Conta encontrada:", account);
+
+    if(!account) {
+        alert("Opps! Verifique o usuário ou a senha.");
+        return;
+    }
+
+    if(account) {
+        if(account.password !== password) {
+            alert("Opps! Verifique o usuário ou a senha.");
+            return;
+        }
+
+        saveSession(email, checksession);
+        
+        window.location.href = "home.html";
+    }
+
+});
 
 //criar conta
 document.getElementById("create-form").addEventListener("submit", function (e) {
@@ -31,6 +66,39 @@ document.getElementById("create-form").addEventListener("submit", function (e) {
 
 });
 
+
+function checkLogged(){
+    if(session) {
+        sessionStorage.setItem("logged", session);
+        logged = session;
+    }
+
+
+    if(logged){
+        saveSession(logged, session)
+
+        window.location.href = "home.html";
+    }
+}
+
 function saveAccount(data) {
-    localStorage.setItem(data.email, JSON.stringify(data));
+    localStorage.setItem(data.login, JSON.stringify(data));
+}
+
+function saveSession(data, saveSession){
+    if(saveSession){
+        localStorage.setItem("session", data);
+    }
+
+    sessionStorage.setItem("logged", data);
+}
+
+function getAccount(key){
+    const account = localStorage.getItem(key);
+
+    if(account){
+        return JSON.parse(account);
+    }
+
+    return "";
 }
